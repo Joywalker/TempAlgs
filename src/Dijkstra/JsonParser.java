7d16fe2730fb6914114b205aed51ba880b73542a
Joywalker <alexandrurazvant525@gmail.com>
2017-05-05 13:32:48 +0300

    Added JsonParser for dijkstra

---
+++ src/Dijkstra/JsonParser.java
@@ -0,0 +1,11 @@
+package Dijkstra;
+
+/**
+ * Created by Joywalker on 5/5/2017.
+ */
+public class JsonParser
+{
+    /**
+     * TODO : implement json parser to iterate through JSON object and Add Edge to the graph.
+      */
+}
